b7ab87c05415316d092a9733b18da1f9cf21e772
meh-who <62934421+meh-who@users.noreply.github.com>
2021-03-06 10:24:42 -0500

    add unity project

---
+++ ARCADE_UI/Assets/getColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getColor : MonoBehaviour
+{
+    public GameObject wheel;
+    public Color colorFromWheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorFromWheel = wheel.GetComponent<ColorWheelControl>().Selection;
+    }
+}
---
+++ ARCADE_UI/Assets/ui/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public LeanTweenType easeType;
+    public float duration;
+    public float inPos;
+    public float outPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveIn()
+    {
+        LeanTween.moveX(gameObject, inPos, duration).setEase(easeType);
+    }
+
+    public void MoveOut()
+    {
+        LeanTween.moveX(gameObject, outPos, duration).setEase(easeType);
+    }
+}
